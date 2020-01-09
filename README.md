0613bc164e3ca088387121d3081252bc6642d852
L.Answer <answer_ljm@163.com>
2020-01-09 01:29:06 -0800

    logrus

--- README.md
+++ README.md
@@ -2,6 +2,19 @@
 
 ___
 
+&nbsp;
+
+# 使用 logrus 日志框架
+```bash
+# 获取代码, 在 $GOPATH/src/github.com目录下
+go get github.com/sirupsen/logrus
+# eg. C:\Users\answer\go\src\github.com\sirupsen\logrus
+
+
+```
+
+
+
 ```bash
 # 新增对应的 ***.proto 文件， 执行以下命令
 protoc.exe  --go_out=./  common.ptoto
@@ -9,6 +22,7 @@ protoc.exe  --go_out=./  common.ptoto
 # 生成对应的 ***.ptoto.pb.go 文件
 ```
 
+&nbsp;
 
 # Reference
  - [windows 下 goprotobuf 的安装与使用](https://blog.csdn.net/u010979642/article/details/103896533)
--- protos/Application.go
+++ protos/Application.go
@@ -5,7 +5,29 @@ import (
 	"jaemon-go/protos/common"
 
 	"jaemon-go/utils"
+
+	logger "github.com/sirupsen/logrus"
+	"os"
 )
+
+// 初始化信息
+func init() {
+	fmt.Println("初始化参数")
+	// logrus 默认的日志输出有 time、level 和 msg 3个 Field，其中 time 可以不显示，方法如下：
+	//logger.SetFormatter(&logger.TextFormatter{DisableTimestamp: true})
+
+	// 设置输出样式，自带的只有两种样式logrus.JSONFormatter{}和logrus.TextFormatter{}
+	logger.SetFormatter(&logger.JSONFormatter{
+		TimestampFormat: "2006-01-02 15:04:05",
+		PrettyPrint: false,
+	})
+	// 设置output,默认为stderr,可以为任何io.Writer，比如文件*os.File
+	logger.SetOutput(os.Stdout)
+	// 设置日志级别为 info 以上
+	logger.SetLevel(logger.InfoLevel)
+	// 显示行号等信息
+	logger.SetReportCaller(true)
+}
 
 func main() {
 	var name string = "answer"
@@ -21,7 +43,12 @@ func main() {
 
 	fmt.Println(string(response.Result))
 
-	fmt.Println("Start Appliaction...")
+	fmt.Println()
+	logger.WithFields(
+		logger.Fields{
+			"key": "jaemon",
+		}).Info("Start Appliaction...")
+	fmt.Println()
 
 	utils.Execute()
 
